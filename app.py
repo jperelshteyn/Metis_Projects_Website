9c5239a8dbb43a8a812f644b6c109126e585daf7
Jason Perelshteyn <j.b.shteyn@gmail.com>
2015-11-11 22:36:18 -0500

    Wired dropdown from code behind and ajax

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 __author__ = 'jperelshteyn'
 
-from flask import Flask, render_template, request, redirect, url_for, abort, session
+from flask import Flask, render_template, request, redirect, url_for, abort, session, jsonify
+import process_headlines
+import time
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -16,5 +19,35 @@ def mta():
 def movies():
     return render_template('movies.html')
 
+@app.route('/twitter_news', methods=["GET", "POST"])
+def twitter_news():
+    option_list = get_headlines()
+    return render_template('twitter_news.html', option_list=option_list)
+
+
+@app.route('/_ajax_handler')
+def ajax_handler():
+    s = request.args.get('selection')
+    return jsonify(result=s)
+
+
+def get_headlines():
+	one_day_ago = time.time() - 8640
+	one_week_ago = time.time() - 60480 
+	client = MongoClient()
+	db = client.twitter_news
+	news_coll = db.news
+	scores_coll = db.headline_scores
+	cursor = news_coll.find({"time": {"$gt": one_day_ago}, "time": {"$lt": one_week_ago}})
+	headlines = []
+	for item in cursor:
+	    h_id = item['_id']
+	    h_text = item['headline']
+	    scores = scores_coll.find_one({'headline_id': h_id})
+	    if scores['s_score'] > 0 and len(scores['f_score']) > 1:
+	        headlines.append({'text': h_text, 'id': h_id})
+	return headlines
+
+
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
